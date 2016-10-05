650c65ae153881d93067b309c905539a79df8648
Steven C. Howell <steven.howell@nist.gov>
2016-10-05 17:00:01 -0400

    begun using box size for each frame

--- tools/calc_gr/calc_gr_from_trajectory.py
+++ tools/calc_gr/calc_gr_from_trajectory.py
@@ -1,17 +1,16 @@
 import os
 import sys
-import string
-import locale
-import numpy
-import math
-import random
+import numpy as np
 import matplotlib.pyplot as plt
 import sasmol.sasmol as sasmol
 
 sys.path.append('./')
 import gr as fortran_gr
 
+
 def get_box_length_list(xst_file_name, stride, sigma=1):
+    import string
+    import locale
 
     boxlength_list = []
 
@@ -25,9 +24,10 @@ def get_box_length_list(xst_file_name, stride, sigma=1):
 
     return boxlength_list
 
-def update_gr(xcoor, ycoor, zcoor, box_length, nbins, natoms2, deltag):
 
-    tgr = numpy.zeros(nbins, numpy.float)
+def update_gr(xcoor, ycoor, zcoor, box_length, n_bins, natoms2, delta_g):
+
+    tgr = np.zeros(n_bins, np.float)
 
     for i in xrange(natoms2 - 1):
         for j in xrange(i + 1, natoms2):
@@ -40,116 +40,100 @@ def update_gr(xcoor, ycoor, zcoor, box_length, nbins, natoms2, deltag):
             yr = yr - box_length * ((yr / box_length).round())
             zr = zr - box_length * ((zr / box_length).round())
 
-            r = math.sqrt((xr * xr) + (yr * yr) + (zr * zr))
+            r = np.sqrt((xr * xr) + (yr * yr) + (zr * zr))
 
             if (r < box_length / 2.0):
-                ig = int(r / deltag)
+                ig = int(r / delta_g)
                 tgr[ig] = tgr[ig] + 2
 
     return tgr
 
 
-def main(pdb_file_name, dcd_file_name, xst_file_name, stride, sigma, nskip=0,
-         show=False):
+def main(pdb_fname, run_log_fname, stride=1, sigma=1, dcd_fname=None,
+         n_skip=0, show=False):
 
-    m1 = sasmol.SasMol(0)
-    m1.read_pdb(pdb_file_name)
+    box_mol = sasmol.SasMol(0)
+    box_mol.read_pdb(pdb_fname)
 
-    if(dcd_file_name != False):
-        dcdfile = m1.open_dcd_read(dcd_file_name)
-        number_of_frames = dcdfile[2]
+    if dcd_fname:
+        dcd_file = box_mol.open_dcd_read(dcd_fname)
+        n_frames = dcd_file[2]
     else:
-        number_of_frames = 1
+        n_frames = 1
 
-    print '> found ', number_of_frames, ' frames in dcd file'
+    run_log = np.loadtxt(run_log_fname)
 
-    if(xst_file_name != False):
-        # box_length_list = get_box_length_list(xst_file_name, stride, sigma)
-        box_length = numpy.loadtxt(xst_file_name)
-
-    #box_length_list = [box_length]
-
-    assert len(box_length) == number_of_frames + 1, (
+    assert len(run_log) == n_frames + 1, (
         '# dcd should be one less than the lengeth of box length: ',
         'len(box_length_list), number_of_frames = {}, {}'.format(
-            len(box_length), number_of_frames))
+            len(run_log), n_frames))
 
-    print 'box_length = ', box_length[0]
-    print 'box_length = ', box_length[-1]
+    box_length = run_log[:, 1]
+    print 'box_length: [ fist last] = {}'.format(box_length[[0, -1]])
 
-    # # not sure what this was for
-    # min_box_length = min(box_length)
-    # max_box_length = max(box_length)
+    n_atoms = box_mol.natoms()
+    n_bins = 1000
+    delta_g = box_length / (2.0 * n_bins)
+    rho = n_atoms / box_length ** 3.0
+    n_frames_used = n_frames - n_skip
+    gr_all = np.empty((n_frames_used, n_bins))  # one g(r) for ecah dcd frame
+    gr = np.empty((n_bins, 2))
+    r = np.empty(n_bins)
 
-    # print 'mininum box length = ', min_box_length
-    # print 'maximum box length = ', max_box_length
+    fsum_gr = np.zeros(n_bins, np.float)
+    fgr = np.zeros(n_bins, np.float)
 
-    # print '2 PI / boxlength = ', 2.0 * numpy.pi / min_box_length
+    count = 0
+
 
     box_length_sum = 0.0
 
-    natoms = m1.natoms()
+    bin_index = np.arange(n_bins) + 1
+    bin_volume = np.outer(delta_g ** 3, (bin_index + 1) ** 3 - bin_index ** 3)
 
-    print 'number of atoms = ', natoms
+    # for i in xrange(n_skip, n_frames):
+    for i in xrange(n_skip, n_skip + 10):
+        sys.stdout.flush()
 
-    nbins = 1000
-    deltag = max_box_length / (2.0 * nbins)
+        if dcd_fname:
+            box_mol.read_dcd_step(dcd_file, i)
 
-    print '> number of bins = ', nbins
-    print '> delta g(r) = ', deltag
+        x_coor = box_mol.coor()[0, :, 0] * sigma
+        y_coor = box_mol.coor()[0, :, 1] * sigma
+        z_coor = box_mol.coor()[0, :, 2] * sigma
 
-    sum_gr = numpy.zeros(nbins, numpy.float)
-    fsum_gr = numpy.zeros(nbins, numpy.float)
-    gr = numpy.zeros(nbins, numpy.float)
-    fgr = numpy.zeros(nbins, numpy.float)
-    r = numpy.zeros(nbins, numpy.float)
+        gr_all[i] = fortran_gr.update_gr(x_coor, y_coor, z_coor, box_length[i],
+                                    n_bins, delta_g[i])
+
+        for i in xrange(n_bins):
+            nid = (4.0 / 3.0) * np.pi * bin_volume * rho
+
+    gr[:, 0] = r
+    gr[:, 1] = np.mean(gr_all, axis=0)
+
+    for i in xrange(n_bins):
+        r[i] = delta_g * (i + 0.5)
+        fgr[i] = fsum_gr[i] / (count * n_atoms * nid)
+
+
+    np.savetxt(gr, 'test{}.dat'.format(stride))
+
+
+
+def plot_gr(gr):
 
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
     ax1.set_ylabel('g(r)')
     ax1.set_xlabel('r')
-
-    count = 0
-
-
-    for i in xrange(nskip, number_of_frames):
-        print i + 1,
-        sys.stdout.flush()
-        if(dcd_file_name != False):
-            m1.read_dcd_step(dcdfile, i)
-        xcoor = m1.coor()[0, :, 0]*sigma
-        ycoor = m1.coor()[0, :, 1]*sigma
-        zcoor = m1.coor()[0, :, 2]*sigma
-
-        ftgr = numpy.zeros(nbins, numpy.float)
-        ftgr = fortran_gr.update_gr(
-            xcoor, ycoor, zcoor, box_length[i], nbins, deltag)
-        fsum_gr = fsum_gr + ftgr
-
-        box_length_sum += box_length[i]
-        count += 1
-
-    average_box_length = box_length_sum / count
-
-    rho = natoms / (average_box_length**3.0)
-
-    for i in xrange(nbins):
-        r[i] = deltag * (i + 0.5)
-        vb = (((i + 1)**3) - (i**3)) * (deltag**3)
-        nid = (4.0 / 3.0) * numpy.pi * vb * rho
-        fgr[i] = fsum_gr[i] / (count * natoms * nid)
-
     line, = ax1.plot(r, fgr, color='red', lw=3)
 
     plt.savefig('test_500to1000_by' + str(stride) + '.png')
+
     if show:
         plt.show()
     else:
         plt.close('all')
-
-    with open('test' + str(stride) + '.dat', 'w') as outfile:
-        for i in range(len(r)):
-            outfile.write('%f\t%f\n' % (r[i], fgr[i]))
 
 
 if __name__ == '__main__':
@@ -157,14 +141,14 @@ if __name__ == '__main__':
     sigma = 3.405
 
     run_path = '../../simulations/lj_sphere_monomer/runs/p_0p14/output'
-    pdb_file_name = 'run1.pdb'
+    pdb_fname = 'run1.pdb'
     dcd_file_name = 'run1_mod.dcd'
     xst_file_name = 'box_length.txt'
-    pdb_file_name = op.join(run_path, pdb_file_name)
+    pdb_fname = op.join(run_path, pdb_fname)
     dcd_file_name = op.join(run_path, dcd_file_name)
     xst_file_name = op.join(run_path, xst_file_name)
 
-    stride = 1
+    gr = main(pdb_fname, xst_file_name, sigma=sigma, dcd_fname=dcd_file_name,
+              n_skip=1000)
 
-    main(pdb_file_name, dcd_file_name, xst_file_name, stride, sigma,
-         nskip=1000)
+    plot_gr(gr)
