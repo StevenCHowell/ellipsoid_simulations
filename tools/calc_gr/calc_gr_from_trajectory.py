19c91973c4b44fe4506bfc887667191b1c464c8b
Steven C. Howell <steven.howell@nist.gov>
2016-10-17 12:26:52 -0400

    replace slow python version with fast python version

--- tools/calc_gr/calc_gr_from_trajectory.py
+++ tools/calc_gr/calc_gr_from_trajectory.py
@@ -2,6 +2,7 @@ import logging
 import os
 import sys
 import numpy as np
+from scipy.spatial.distance import pdist
 import sasmol.sasmol as sasmol
 
 sys.path.append('./')
@@ -11,7 +12,7 @@ FORMAT = "%(asctime)-15s: %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 
 
-def update_gr(xcoor, ycoor, zcoor, box_length, n_bins, n_atoms, delta_r):
+def slow_update_gr(xcoor, ycoor, zcoor, box_length, n_bins, n_atoms, delta_r):
 
     gr = np.zeros(n_bins, np.float)
 
@@ -33,6 +34,23 @@ def update_gr(xcoor, ycoor, zcoor, box_length, n_bins, n_atoms, delta_r):
                 gr[ig] += 2
 
     return gr
+
+
+def calc_gr(coor, box_length, gr, dr):
+
+    dx = pdist(coor[:, :1])
+    dy = pdist(coor[:, 0:2])
+    dz = pdist(coor[:, 1:])
+
+    dx -= box_length * ((dx / box_length).round())
+    dy -= box_length * ((dy / box_length).round())
+    dz -= box_length * ((dz / box_length).round())
+
+    r = np.sqrt((dx * dx) + (dy * dy) + (dz * dz))
+
+    r_i = (r[r < box_length / 2.0] / dr).astype(int)  # round down
+    r_i_unique = np.unique(r_i, return_counts=True)
+    gr[r_i_unique[0]] += 2 * r_i_unique[1]
 
 
 def main(pdb_fname, run_log_fname, stride=1, sigma=1, dcd_fname=None,
@@ -70,43 +88,33 @@ def main(pdb_fname, run_log_fname, stride=1, sigma=1, dcd_fname=None,
     gr_all = np.zeros((n_gr, n_bins))  # one g(r) for ecah dcd frame
     gr = np.zeros((n_bins, 2))
 
-    # # using a different r_grid for each frame
-    # delta_r = box_length / (2.0 * n_bins)
-    # bin_index = np.arange(n_bins) + 1
-    # rho = (n_atoms / box_length ** 3.0).reshape(-1, 1)  # particle density
-
-    # bin_volume = np.outer(delta_r ** 3, (bin_index + 1) ** 3 - bin_index ** 3)
-    # r = np.outer(delta_r, (bin_index + 0.5))
-    # n_ideal = (4.0 / 3.0) * np.pi * bin_volume * rho  # expected n for ideal gas
-
     # using the same r_grid for each frame
     dr = box_length[n_skip:].max() / (2.0 * n_bins)  # delg in F&S
     bin_index = np.arange(n_bins) + 1
     rho = (n_atoms / (box_length ** 3.0)).reshape(-1, 1)  # frame specific density
 
-    # bin_volume = ((bin_index + 1) ** 3 - bin_index ** 3) * delta_r ** 3
     r = (bin_index - 0.5) * dr
-    bin_volume = 4.0 / 3.0 * np.pi * ((r + dr / 2) ** 3 -
-                                      (r - dr / 2) ** 3)
+    bin_volume = 4.0 / 3.0 * np.pi * ((r + dr / 2) ** 3 - (r - dr / 2) ** 3)
     n_ideal = bin_volume * rho  # expected n for ideal gas
 
-    # for i in xrange(n_skip, n_frames):
+    if n_skip:
+        for i in xrange(n_skip):
+            # read and throw away these coordinates
+            box_mol.read_dcd_step(dcd_file, i)
+
     for i in xrange(n_skip, n_frames):
         sys.stdout.flush()
 
-        if dcd_fname:
+        try:
             box_mol.read_dcd_step(dcd_file, i)
+        except NameError:
+            print('calculating g(r) for {}'.format(pdb_fname))
 
-        x_coor = box_mol.coor()[0][:, 0] * sigma
-        y_coor = box_mol.coor()[0][:, 1] * sigma
-        z_coor = box_mol.coor()[0][:, 2] * sigma
-
-        gr_all[i-n_skip] = fortran_gr.update_gr(x_coor, y_coor, z_coor,
-                                                box_length[i], n_bins, dr)
-        # gr_all[i-n_skip] = update_gr(x_coor, y_coor, z_coor, box_length[i], n_bins,
-                              # n_atoms, delta_r)
+        coor = box_mol.coor()[0] * sigma
+        calc_gr(coor, box_length[i], gr_all[i-n_skip], dr)
         gr_all[i-n_skip] /= n_ideal[i]  # normalize expected n for ideal gas
 
+    box_mol.close_dcd_read(dcd_file)
     gr[:, 0] = r
     gr[:, 1] = np.mean(gr_all, axis=0) / n_atoms  # normalize by frames and atoms
 
